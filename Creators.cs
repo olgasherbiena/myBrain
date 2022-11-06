e02a8ae065118bb404b1ad5ec9aa3258fe8240a4
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-11-06 12:27:03 +0100

    Update Creators.cs

--- Creators.cs
+++ Creators.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 
-class Creators
+class Creators // клас розробників
 {
-    public string initials;
-    public int group_number;
-    public string faculty;
-    public string task;
+    public struct student
+    {
+        public string initials;
+        public int group_number;
+        public string faculty;
+        public string task;
+    }
+    student[] people;
     Creators()
     {
-        initials = "Name";
-        group_number = 100;
-        faculty = "Faculty";
-        task = "Task";
+        people = new student[5];
+        student.initials = "Name";
+        student.group_number = 100;
+        student.faculty = "Faculty";
+        student.task = "Task";
     }
     public void Print() { }
 }
