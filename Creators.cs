5b807b6b9c253e89b329e361fa6010511d7880cb
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-11-06 12:26:31 +0100

    Add files via upload

---
+++ Creators.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Creators
+{
+    public string initials;
+    public int group_number;
+    public string faculty;
+    public string task;
+    Creators()
+    {
+        initials = "Name";
+        group_number = 100;
+        faculty = "Faculty";
+        task = "Task";
+    }
+    public void Print() { }
+}
