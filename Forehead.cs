485767adc7b5cddb09e7ab27034c08552f4763f3
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-11-07 11:47:35 +0100

    Update Forehead.cs

--- Forehead.cs
+++ Forehead.cs
@@ -8,10 +8,12 @@ namespace Brain
 {
     public class Forehead:IPrint
     {
+        public int id;
         public information frh_inf;
         public string forehead_part; // зберігається загальна інформація про лобову частину (та що одразу після заголовку)
         public Forehead()
         {
+            id=120;
             forehead_part = "";
             frh_inf.structure = "";
             frh_inf.latin = "";
