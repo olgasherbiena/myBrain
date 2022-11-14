06e8d0d845e2ccbe951a7ad878fa6247217bdb85
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-11-14 11:43:29 +0100

    Update Crown.cs

--- Crown.cs
+++ Crown.cs
@@ -9,7 +9,6 @@ namespace Brain
     public class Crown:Part, IPrint
     {
         public int id;
-        public information inf_crw;
         public string crown_part; // зберігається загальна інформація про тім'яну частину (та що одразу після заголовку)
         public string lateral_intraparietal_area;
         public string ventral_intraparietal_area;
@@ -19,14 +18,14 @@ namespace Brain
         {
             id=110;
             crown_part = "";
-            inf_crw.structure = "";
-            inf_crw.latin = "";
-            inf_crw.functions = "";
+            structure = "";
+            latin = "";
+            functions = "";
             lateral_intraparietal_area = "";
             ventral_intraparietal_area = "";
             medial_intraparietal_area = "";
             front_intraparietal_area = "";
-            inf_crw.clinical_significance = "";
+            clinical_significance = "";
         }
         public void Print(string str) { }
         public void Print_all_information() { }
