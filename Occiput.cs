9bf25283b67f1bc9b72cd9e88d7a05cd1b8175d3
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-11-14 11:46:44 +0100

    Update Occiput.cs

--- Occiput.cs
+++ Occiput.cs
@@ -9,7 +9,6 @@ namespace Brain
     public class Occiput:Part, IPrint
     {
         public int id;
-        public information inf_occ;
         public string occiput; // зберігається загальна інформація про потиличну частину (та що одразу після заголовку)
         public string first_functional_area;
         public string ventral_flow;
@@ -18,13 +17,13 @@ namespace Brain
         {
             id=102;
             occiput = "";
-            inf_occ.structure = "";
-            inf_occ.latin = "";
-            inf_occ.functions="";
+            structure = "";
+            latin = "";
+            functions="";
             first_functional_area="";
             ventral_flow="";
             dorsomedial_flow="";
-            inf_occ.clinical_significance="";
+            clinical_significance="";
 
         }
         public void Print(string str) { }
