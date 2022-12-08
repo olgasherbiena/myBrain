8788591b238a7f28def9408d1a9b5c432418d7cb
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-12-08 15:04:09 +0100

    Update Temporal.cs

--- Temporal.cs
+++ Temporal.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,30 +6,37 @@ using System.Threading.Tasks;
 
 namespace Brain
 {
-    public class Temporal:Part, IPrint
+    public class Temporal: Part, IPrint
     {
-        public int id;
-        public string temporal_name; // зберігається загальна інформація про скроневу частину (та що одразу після заголовку)
-        public struct touch_input_processing // обробка сенсорного введення
+        public const int id = 202;
+        private string temporal_name; // зберігається загальна інформація про скроневу частину (та що одразу після заголовку)
+        private struct touch_input_processing // обробка сенсорного введення
         {
             public string otic;
             public string visual;
             public string speech_recognition;
         }
-        public touch_input_processing function;
+        private touch_input_processing function;
         public Temporal()
         {
-            id=202;
-            temporal_name = "";
-            structure = "";
-            latin = "";
-            functions = "";
-            function.otic = "";
-            function.visual = "";
-            function.speech_recognition = "";
-            clinical_significance = "";
+            temporal_name = "tmp_part";
+            structure = "tmp_struct";
+            latin = "tmp_lat";
+            functions = "tmp_func";
+            function.otic = "tmp1";
+            function.visual = "tmp2";
+            function.speech_recognition = "tmp3";
+            clinical_significance = "tmp4";
         }
         public void Print(string str) { }
         public void Print_all_information() { }
+        public string Temporal_name { set { temporal_name = value; } get { return temporal_name; } }
+        public string Otic { set { function.otic = value; } get { return function.otic; } }
+        public string Visual { set { function.visual = value; } get { return function.visual; } }
+        public string Speech_recognition { set { function.speech_recognition = value; } get { return function.speech_recognition; } }
+        public override string ToString()
+        {
+            return $"{id}; Name: {this.temporal_name}; Latin: {this.latin}; Functions: {this.functions}";
+        }
     }
 }
