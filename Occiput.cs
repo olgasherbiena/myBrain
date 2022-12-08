882a614b86ec28d63820f11db84389074809c6ba
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-12-08 15:03:13 +0100

    Update Occiput.cs

--- Occiput.cs
+++ Occiput.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,25 +8,32 @@ namespace Brain
 {
     public class Occiput:Part, IPrint
     {
-        public int id;
-        public string occiput_name; // зберігається загальна інформація про потиличну частину (та що одразу після заголовку)
-        public string first_functional_area;
-        public string ventral_flow;
-        public string dorsomedial_flow;
+        public const int id = 102;
+        private string occiput_name; // зберігається загальна інформація про потиличну частину (та що одразу після заголовку)
+        private string first_functional_area;
+        private string ventral_flow;
+        private string dorsomedial_flow;
         public Occiput()
         {
-            id=102;
-            occiput_name = "";
-            structure = "";
-            latin = "";
-            functions="";
-            first_functional_area="";
-            ventral_flow="";
-            dorsomedial_flow="";
-            clinical_significance="";
+            occiput_name = "occ_part";
+            structure = "occ_struct";
+            latin = "occ_lat";
+            functions = "occ_func";
+            clinical_significance = "occ_4";
+            first_functional_area = "occ_1";
+            ventral_flow = "occ_2";
+            dorsomedial_flow = "occ_3";
 
         }
         public void Print(string str) { }
         public void Print_all_information() { }
+        public string Occiput_name { set { occiput_name = value; } get { return occiput_name; } }
+        public string First_functional_area { set { first_functional_area = value; } get { return first_functional_area; } }
+        public string Ventral_flow { set { ventral_flow = value; } get { return ventral_flow; } }
+
+        public override string ToString()
+        {
+            return $"{id}; Name: {this.occiput_name}; Latin: {this.latin}; Functions: {this.functions}";
+        }
     }
 }
