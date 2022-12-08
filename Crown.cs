d6b68a57f05f14ac1afa9f70f58555ba26c9736a
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-12-08 15:00:04 +0100

    Update Crown.cs

--- Crown.cs
+++ Crown.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,28 +6,36 @@ using System.Threading.Tasks;
 
 namespace Brain
 {
-    public class Crown:Part, IPrint
+    public class Crown:Part,IPrint
     {
-        public int id;
-        public string crown_name; // зберігається загальна інформація про тім'яну частину (та що одразу після заголовку)
-        public string lateral_intraparietal_area;
-        public string ventral_intraparietal_area;
-        public string medial_intraparietal_area;
-        public string front_intraparietal_area;
+        public const int id = 110;
+        private string crown_name; // зберігається загальна інформація про тім'яну частину (та що одразу після заголовку)
+        private string lateral_intraparietal_area;
+        private string ventral_intraparietal_area;
+        private string medial_intraparietal_area;
+        private string front_intraparietal_area;
         public Crown()
         {
-            id=110;
-            crown_part = "";
-            structure = "";
-            latin = "";
-            functions = "";
-            lateral_intraparietal_area = "";
-            ventral_intraparietal_area = "";
-            medial_intraparietal_area = "";
-            front_intraparietal_area = "";
-            clinical_significance = "";
+            crown_name = "crw_part";
+            structure = "crw_struct";
+            latin = "crw_lat";
+            functions = "crw_func";
+            clinical_significance = "crw_clinic";
+            lateral_intraparietal_area = "crw_1";
+            ventral_intraparietal_area = "crw_2";
+            medial_intraparietal_area = "crw_3";
+            front_intraparietal_area = "crw_4";
         }
         public void Print(string str) { }
         public void Print_all_information() { }
+        public string Crown_name { set { crown_name = value; } get { return crown_name; } }
+        public string Lateral_intraparietal_area { set { lateral_intraparietal_area = value; } get { return lateral_intraparietal_area; } }
+        public string Ventral_intraparietal_area { set { ventral_intraparietal_area = value; }get { return ventral_intraparietal_area; } }
+        public string Medial_intraparietal_area { set { medial_intraparietal_area = value; } get { return medial_intraparietal_area; } }
+        public string Front_intraparietal_area { set { front_intraparietal_area = value; } get { return front_intraparietal_area; } }
+        public override string ToString()
+        {
+            return $"{id}; Name: {this.crown_name}; Latin: {this.latin}; Functions: {this.functions}";
+        }
     }
 }
