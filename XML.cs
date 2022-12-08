c3b0ed73987a200bae09f3b53400d31b9213775f
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-12-08 15:04:43 +0100

    Update XML.cs

--- XML.cs
+++ XML.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,7 +16,9 @@ namespace Brain
             filenameLoad = Load;
             filenameUpload = Upload;
         }
-        public void ForLoad(string filenameLoad, Brain brain) { } // функція для запису в файл
-        public void ForUpload(string filenameUpload, Brain brain) { } // функція для зчитування з файлу 
+       public void ForLoad(string filenameLoad, Brain brain) { } // функція для запису в файл
+       public void ForUpload(string filenameUpload, Brain brain) { } // функція для зчитування з файлу
+
     }
 }
+
