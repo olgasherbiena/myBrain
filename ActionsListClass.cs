37114c4b14230c6eb028084756dfcdaaf0446a97
Vladislavich7 <102238970+Vladislavich7@users.noreply.github.com>
2022-12-08 14:36:06 +0100

    Update ActionsListClass.cs

--- ActionsListClass.cs
+++ ActionsListClass.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,18 +18,31 @@ namespace Brain
             //actions.Add("Ходьба", new int[5] { 101, 202, 110, 120, 102 }); // 101, 202 ... це id частин мозку
             //actions.Add("Задача зі стереометрії", new int[5] { 202, 101, 120, 110, 102 });
             actions = new Dictionary<string, Dictionary<int, int>>();
-            actions.Add("Ходьба", new Dictionary<int, int>{
-                { 101, 1000 },
-                { 202, 2000 },
-                { 110, 2000 },
-                { 120, 1000 },
-                { 102, 1000 } });
-            actions.Add("Задача зі стереометрії", new Dictionary<int, int>{
-                { 202, 2000 },
-                { 101, 1000 },
+            actions.Add("Ходьба", new Dictionary<int, int>{ 
+                { 101, 3000 },
+                { 120, 3000 } });
+            actions.Add("Моторика рук", new Dictionary<int, int>{
+                { 120, 3000 } });
+            actions.Add("Зір", new Dictionary<int, int>{
+                { 102, 3000 } });
+            actions.Add("Пам'ять", new Dictionary<int, int>{
                 { 120, 3000 },
-                { 110, 1000 },
-                { 102, 1000 } });
+                { 202, 3000 } });
+            actions.Add("Мовлення", new Dictionary<int, int>{
+                { 120, 3000 } });
+            actions.Add("Слух", new Dictionary<int, int>{
+                { 110, 3000 } });
+            actions.Add("Смак", new Dictionary<int, int>{
+                { 110, 3000 },
+                { 120, 3000 } });
+            actions.Add("Інтелект", new Dictionary<int, int>{
+                { 110, 3000 } });
+            actions.Add("Емоції", new Dictionary<int, int>{
+                { 120, 3000 },
+                { 202, 3000 } });
+            actions.Add("Сон", new Dictionary<int, int>{
+                { 120, 3000 } });
         }
     }
+
 }
